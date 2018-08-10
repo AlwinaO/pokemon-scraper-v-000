323a6670298abeae93d2585255110df92f4d120f
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-08-10 00:34:57 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,7 +15,7 @@ class Pokemon
 
   def self.find(id, db)
     db.execute("SELECT * FROM pokemon WHERE id = ?", id)
-    self.new(id:, name:, type:, db: @db)
+    self.new(id, name, type, db)
 
   end
 end
